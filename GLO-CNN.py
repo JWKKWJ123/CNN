65f2bac4fbd8a225de5e16d34f89503bd7e6b8bc
JWKKWJ123 <120324566+JWKKWJ123@users.noreply.github.com>
2023-07-14 20:35:32 +0200

    Add files via upload

--- GLO-CNN.py
+++ GLO-CNN.py
@@ -5,11 +5,7 @@ import torch.optim as optim
 import time
 from torchsummary import summary
 
-
-
-
-
-
+#convolution block (3 convolution layers) 
 class vgg_block(nn.Module):
     def __init__(self, inplanes, planes):
         super(vgg_block, self).__init__()
@@ -37,12 +33,6 @@ class vgg_block(nn.Module):
         return out
 
 
-
-
-
-
-
-
         
 class Global(nn.Module):
     def __init__(self):
@@ -51,58 +41,36 @@ class Global(nn.Module):
         self.conv1 = nn.Conv3d(1, 16, kernel_size=3, stride=(2, 2, 2), padding=1, bias=False)
         self.bn1 = nn.BatchNorm3d(16)
         self.relu = nn.ReLU(inplace=True)
-        #self.maxpool1 = nn.MaxPool3d(kernel_size=(3, 3, 3), stride=2, padding=1)
+		#The input and output of vgg_block( ) is the number of channels
+		#4 convolution blocks
         self.block1 = vgg_block(16, 16)
         self.block2 = vgg_block(32, 32)
         self.block3 = vgg_block(48, 48)
         self.block4 = vgg_block(64, 64)
+		#FC layers
         self.conv_cls = nn.Sequential(
             nn.AdaptiveMaxPool3d(output_size=(1, 1, 1)),
             nn.Flatten(start_dim=1),
-            #nn.LazyLinear(512),
             nn.Linear(80,32),
             nn.ReLU(inplace=True),
             nn.Dropout(0.2),
             nn.Linear(32,1),
             nn.Sigmoid()
             )
-        #self.block1
-        #self.block1
-        #self.block1
-
 
         
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
-        #x = self.maxpool1(x)
         x = self.block1(x)
         x = self.block2(x)
         x = self.block3(x)
         x = self.block4(x)
-        #x = self.layer2(x)
-        #x = self.layer3(x)
-        #x = self.layer4(x)
         x = self.conv_cls(x)
-        #x = torch.sigmoid_(x)
+		#output x is the possibility to be positive [0,1]
         return x
         
-    
-'''
-net = VGG()
-
-X = torch.randn(size=(1,1,137,177,144), dtype=torch.float32)
-
-Y = net(X)
-print(Y.shape)
-
-summary(net,(1,137,177,144))
-
-'''
-
-
-
 
         
         
